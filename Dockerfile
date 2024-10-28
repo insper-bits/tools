16d06df3dd73b7865f7513e2801b0db814bde3eb
Your Name <corsiferrao@gmail.com>
2024-10-28 16:56:53 -0300

    updagte

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,5 @@
 # Use a lightweight Python base image
-FROM python:3.9-slim
+FROM prairielearn/grader-python
 
-# Set working directory inside the container
-WORKDIR /app
-
-# Install git and a minimal shell (sh instead of bash to reduce footprint)
-RUN apt-get update && \
-    apt-get install -y git && \
-    rm -rf /var/lib/apt/lists/*
-
-# Clone the repository
-RUN git clone https://github.com/insper-bits/tools.git 
-
-# Install Python dependencies from requirements.txt if it exists
-RUN pip install -r tools/requirements.txt
-
-# Set entrypoint to sh for minimal shell
-ENTRYPOINT ["/bin/sh"]
-
+RUN dnf install -y git
+RUN pip install git+https://github.com/insper-bits/tools
--- setup.py
+++ setup.py
@@ -3,10 +3,24 @@ from setuptools import setup, find_packages
 setup(
     name="bits",
     version="1.0",
-    #    package_dir={"": "src"},
-    packages=["bits", "bits.hw", "bits.sw"],
+    packages=[
+        "bits",
+        "bits.hw",
+        "bits.util",
+        "bits.sw.assembler",
+        "bits.sw.vmtranslator",
+        "bits.sw.simulator",
+    ],
     include_package_data=True,
-    install_requires=["click", "pyyaml", "pytest", "tabulate", "myhdl"],
+    install_requires=[
+        "click",
+        "pyyaml",
+        "pytest",
+        "tabulate",
+        "myhdl",
+        "wheel",
+        "PyQt5",
+    ],
     entry_points="""
         [console_scripts]
         bits=bits:cli
